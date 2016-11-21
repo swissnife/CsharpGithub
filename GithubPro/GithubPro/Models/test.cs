c3d2c6006d885bc9ae15705831b081175175a24a
swissnife <said@it1.sy>
2016-11-21 13:08:10 +0100

    init nr3

---
+++ GithubPro/GithubPro/Models/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GithubPro.Models
+{
+    public class test
+    {
+        public string Same { get; set; }
+    }
+}
